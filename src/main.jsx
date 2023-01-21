0d62e082fed1867d5535c580423e92312548e41a
issafaqiri <faqiriissa@gmail.com>
2023-01-22 01:35:33 +0430

    create redirects file for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
+    <Provider store={store}>
+      <Router>
         <App />
-      </Provider>
-    </HashRouter>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
